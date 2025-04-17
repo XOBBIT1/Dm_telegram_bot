760fdcadce9922c37f413dbb7bdbcbebf61a39a1
Denis Grishankov <--global>
2025-04-18 00:13:47 +0300

    created project ifrostructure and greetings message

---
+++ app/interfaces/telegram/start/commands/base_command.py
@@ -0,0 +1,19 @@
+from aiogram import types
+from aiogram.fsm.context import FSMContext
+
+from app.interfaces.states.user_states import UserState
+from app.interfaces.telegram.start.keyboards.start_keyboard import keyboard_start
+
+
+async def day_card(message: types.Message, state: FSMContext):
+    await message.answer(
+        f"Привет, <b>{message.from_user.first_name}</b>! 👋\n"
+        "Голоден? Или просто хочешь вкусненького? 😋\n\n"
+        "Здесь ты можешь:\n"
+        "🍔 Посмотреть меню\n"
+        "🛒 Заглянуть в свою корзину\n"
+        "🎟 Ввести промокод на скидку\n\n"
+        "Жми на кнопки ниже — и еда сама тебя найдёт! 👇\n\n"
+        "<b>P.S.</b> Не стесняйся писать мне — я всегда на связи 😎",
+        parse_mode="HTML", reply_markup=keyboard_start())
+    await state.set_state(UserState.any_text)
